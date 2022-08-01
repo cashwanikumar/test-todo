5dd7c02b5f3e81f6dda553bd216e020070c10728
Ashwani <cashwanikumar@gmail.com>
2022-08-01 12:40:02 +0530

    cypress test

---
+++ cypress/e2e/create.cy.js
@@ -0,0 +1,18 @@
+/* eslint-disable promise/always-return */
+/* eslint-disable sonarjs/no-duplicate-string */
+/// <reference types="cypress" />
+
+const baseUrl = Cypress.env('baseUrl');
+
+describe('Todo Page', () => {
+
+  context('test todo', () => {
+    beforeEach(() => {
+      cy.visit(`${baseUrl}`);
+    });
+
+    it('title should be visible', () => {
+        cy.findAllByText('Things To Do').should('have.length', 1)
+    });
+  });
+});
