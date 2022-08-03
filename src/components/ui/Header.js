d7ae115db1fa2270ae23bd875cf6095c9b65ee9b
Ashwani <cashwanikumar@gmail.com>
2022-08-03 15:21:50 +0530

    typo updated

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
     return (
         <header>
             <h1>Things To Do</h1>
-            <h3>Subtitles</h3>
+            <h3>Subtitle note</h3>
             <InputWrapper {...props}/>
         </header>
     );
